37fe0e6bc1fdf322443e187635bb43da25b5e5d7
CyberCaban <andrewgamer63@gmail.com>
2023-03-05 19:57:11 +0300

    db added

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-const multer = require("multer")
-
 const express = require("express")
 const app = express()
-
 const path = require('path')
 
-const upload = multer({ dest: "img" })
+const multer = require("multer")
+
+//датабаза
+var Datastore = require('nedb');
+var db = new Datastore({filename : 'data', timestampData: true});
+db.loadDatabase();
+
+//объяснение мультеру как хранить файл
+const storage = multer.diskStorage({
+    //место сохранения
+    destination: function(req, file, cb){
+        cb(null, 'img')
+    },
+    //имя файла
+    filename: function (req, file, cb){
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, uniqueSuffix + '-' + file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:false}))
@@ -16,6 +33,11 @@ app.get("/", (req, res) => {
 
 app.post("/upload", upload.single("file"), (req, res, next) => {
     console.log(req.file.destination, req.file.filename, req.file.originalname);
+    const fileData = {
+        fileName: req.file.filename,
+        originalname: req.file.originalname
+    }
+    db.insert(fileData)
     res.render("index", { fileLink: `${req.headers.origin}/img/${req.file.filename}`, originalname:`${req.file.originalname}`})
 })
 
