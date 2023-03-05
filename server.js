d237005207e0495d3c2c1b313596a69908c6305f
CyberCaban <andrewgamer63@gmail.com>
2023-03-05 11:02:59 +0300

    working somehow

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const multer = require("multer")
 const express = require("express")
 const app = express()
 
+const path = require('path')
+
 const upload = multer({ dest: "img" })
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({extended:false}))
-
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -15,11 +16,11 @@ app.get("/", (req, res) => {
 
 app.post("/upload", upload.single("file"), (req, res, next) => {
     console.log(req.file.destination, req.file.filename, req.file.originalname);
-    const file = `${__dirname}/text.txt`
-    res.download('/text.txt', 'text.txt', function(error) {
-        console.log(error);
-    })
-    res.send(__dirname)
+    res.render("index", { fileLink: `${req.headers.origin}/img/${req.file.filename}`, originalname:`${req.file.originalname}`})
+})
+
+app.get("/img/:id", (req, res) => {
+    res.download(path.resolve(`./img/${req.params.id}`))
 })
 
 app.listen(3000)
